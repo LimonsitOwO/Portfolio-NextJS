163f0337e54ba520d0dae2966eb4c25a6326a4e8
Luis Carlos <fullstack@crediseguro.co>
2026-02-02 13:24:09 -0500

    Delete all

--- app/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Hero } from "./Hero";
-export { default as AboutMe } from "./AboutMe";
-export { default as Services } from "./Services";
-export { default as Experience } from "./Experience";
-export { default as Knowledge } from "./Knowledge";
-export { default as Technologies } from "./Technologies";
-export { default as ContactMe } from "./ContactMe";
--- components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Wrapper } from './Wrapper';
-export { default as Navbar } from './header/Navbar';
-export { default as Button } from './Button';
-export { default as Footer } from './footer/Footer';
